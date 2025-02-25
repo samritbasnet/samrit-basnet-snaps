d477e39e886b24a2c24e0dbb7d7a236d19ac4ac7
Samrit Basnet <samritbasnet70@gmail.com>
2025-02-25 17:12:56 -0500

    some tweaks changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import "./styles/partials/App.scss";
-
+import "./App.scss";
+import Header from "./components/Header/Header";
 function App() {
   return (
     <>
-      <h1>Snaps</h1>
+      <Header />
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import "./Header.scss";
-function Header() {
+export default function Header() {
   return (
     <div>
       <header className="header">
-        
+        <div className="header__logo">Snaps</div>
+        <div className="header__actions">
+          <nav className="header__nav">
+            <button className="filter-button">
+              <span>Filters</span>
+            </button>
+          </nav>
+        </div>
       </header>
     </div>
   );
