54d453bda69691b1438af4c8989cf43735025f50
Samrit Basnet <samritbasnet70@gmail.com>
2025-02-24 19:15:23 -0500

    partials setup  for snaps sprint-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.scss";
+import "./styles/partials/App";
 
 function App() {
   return (
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import "./Header.scss";
+function Header() {
+  return (
+    <div>
+      <header className="header">
+        
+      </header>
+    </div>
+  );
+}
