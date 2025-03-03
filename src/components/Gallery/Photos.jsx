2c115adfa9b4722c4e521a4cf0cdb863183a1685
Samrit Basnet <samritbasnet70@gmail.com>
2025-03-02 19:21:44 -0500

    style for photographer button fix

--- src/components/Gallery/Photos.jsx
+++ src/components/Gallery/Photos.jsx
@@ -1,29 +1,35 @@
 import photos from "../../Data/photos.json";
-import './Photos.scss';
+import "./Photos.scss";
 
-const Photos=({selectedTag,isTagPanelOpen})=>{
-    const filteredPhotos=selectedTag ? photos.filter((photo)=>photo.tags.includes(selectedTag)) : photos;
-    // const filteredPhotos=photos.filter((photo)=>photo.tags.includes(selectedTag));
+const Photos = ({ selectedTag, isTagPanelOpen }) => {
+  const filteredPhotos = selectedTag
+    ? photos.filter((photo) => photo.tags.includes(selectedTag))
+    : photos;
 
-    console.log(filteredPhotos);
-    console.log(selectedTag);
-    return(
-<div className={`photos ${isTagPanelOpen ? "with-tags-open" : ""}`}>
-    {filteredPhotos.map((photo)=>(
-        <article className='photos__photo' key={photo.id}>
-        <div className="photos__container">
-
-             <img className="photos__image" src={photo.photo} alt={photo.photoDescription} />
-             <div className="photos__author"><p>{photo.photographer}</p></div>
-            {photo.tags.map((tag,index)=>(
-                <p key={index}>{tag}</p>
+  console.log(filteredPhotos);
+  console.log(selectedTag);
+  return (
+    <div className={`photos ${isTagPanelOpen ? "with-tags-open" : ""}`}>
+      {filteredPhotos.map((photo) => (
+        <article className="photos__photo" key={photo.id}>
+          <div className="photos__container">
+            <img
+              className="photos__image"
+              src={photo.photo}
+              alt={photo.photoDescription}
+            />
+            <div className="photos__author">
+              <p>{photo.photographer}</p>
+            </div>
+          </div>
+          <div className="photos__tags">
+            {photo.tags.map((tag, index) => (
+              <p key={index}>{tag}</p>
             ))}
-             </div> 
-         
+          </div>
         </article>
-    ))}
-</div>
-    )
-
-}
+      ))}
+    </div>
+  );
+};
 export default Photos;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import './Hero.scss'
+import "./Hero.scss";
 const Hero = () => {
   return (
-    <section className='hero'>
-        <div className="hero__content">
-<h2 className="hero__title">
-    Our mission:
-</h2>
-<p className="hero__text title" >
-          Provide photographers a space to share photos of the neighborhoods they cherish,{" "}
-          <span className="hero__text--italic">expressed in their unique style.</span>
+    <section className="hero">
+      <div className="hero__content">
+        <h2 className="hero__title">Our mission:</h2>
+        <p className="hero__text title">
+          Provide photographers a space to share photos of the neighborhoods
+          they cherish,{" "}
+          <span className="hero__text--italic">
+            expressed in their unique style.
+          </span>
         </p>
-        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
